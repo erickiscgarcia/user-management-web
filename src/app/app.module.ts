d1bd1da7ba3f10b91b043b711958feaaaaa30de2
Erick Garcia <erick.isc.dante@gmail.com>
2025-08-07 12:59:07 -0600

    Added empty components and service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RegistryComponent } from './components/registry/registry.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserFormComponent } from './components/user-form/user-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegistryComponent,
+    UsersComponent,
+    UserFormComponent
   ],
   imports: [
     BrowserModule,
